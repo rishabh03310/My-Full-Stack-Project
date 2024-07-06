a983dd4f20ea7fc223c302e3851a9d061dc0df44
Rishabh gupta <rishabhguptaa08@gmail.com>
2024-07-06 15:42:02 +0530

    add controllers

--- Src/controllers/user.controller.js
+++ Src/controllers/user.controller.js
@@ -1,10 +1,68 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
+import {ApiError} from "../utils/appError.js"
+import {User} from "../models/user.model.js"
+import {uploadonCloud} from "../utils/FileUpload.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
 
-const registerUser = asyncHandler (async (req , res,)=>{
-    res.status(200).json({
-        Message: "Rishabh Gupta"
+const registerUser = asyncHandler (async (req , res)=>{
+    
+    const {username,email,FullName, password} = req.body
+    console.log("email", email);
+
+    if(
+        [username,email,FullName, password].some((field)=>
+        field?.trim()=== "")
+    ){
+        throw new ApiError(400, "Field is empty")
+    }
+
+    const existdUser = User.find({
+        $or:[{username}, {email}]
     })
-}
-)
 
-export {registerUser}
+    if(existdUser){
+        throw new ApiError(409, "User is already exist")
+    }
+
+    const avatarlocalpath = req.files?.avatar[0]?.path;
+    const coverImagelocalpath = res.files?.coverImage[0]?.path;
+
+    if(!avatarlocalpath){
+        throw new ApiError(400, "Avatar is required")
+    }
+
+    const avatar = await uploadonCloud (avatarlocalpath)
+    const coverImage= await uploadonCloud(coverImagelocalpath)
+
+    if(!avatar){
+        throw new ApiError(400, "Avatar is required");
+    }
+
+    const user = await User.create({
+        username: username.tolowercase(),
+        email,
+        FullName,
+        avatar: avatar.url,
+        coverImage: coverImage?.url || "",
+        password,
+    })
+
+    const createdUser = await user.findbyId(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500, "Something went wrong")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200, createdUser, "user registered Successfully")
+    )
+
+
+
+})
+
+export {
+    registerUser,
+}
